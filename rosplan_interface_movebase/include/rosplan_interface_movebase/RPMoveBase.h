065ca48bdd15f1be8804cd292143b1453addc758
michael <michael.cashmore@kcl.ac.uk>
2015-01-09 18:29:15 +0000

    Added movebase interface

---
+++ rosplan_interface_movebase/include/rosplan_interface_movebase/RPMoveBase.h
@@ -0,0 +1,42 @@
+#include <ros/ros.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <actionlib/client/simple_action_client.h>
+#include "rosplan_dispatch_msgs/ActionDispatch.h"
+#include "rosplan_dispatch_msgs/ActionFeedback.h"
+#include "move_base_msgs/MoveBaseAction.h"
+#include "mongodb_store/message_store.h"
+#include "geometry_msgs/PoseStamped.h"
+
+
+#ifndef KCL_movebase
+#define KCL_movebase
+
+/**
+ * This file defines the RPMoveBase class.
+ * RPMoveBase is used to connect ROSPlan to the MoveBase library.
+ * PDDL "goto_waypoint" actions become "move_base_msgs::MoveBase" actions.
+ * Waypoint goals are fetched by name from the SceneDB (implemented by mongoDB).
+ */
+namespace KCL_rosplan {
+
+	class RPMoveBase
+	{
+
+	private:
+
+		mongodb_store::MessageStoreProxy message_store;
+		actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> action_client;
+		ros::Publisher action_feedback_pub;
+
+	public:
+
+		/* constructor */
+		RPMoveBase(ros::NodeHandle &nh, std::string &actionserver);
+
+		/* listen to and process action_dispatch topic */
+		void dispatchCallback(const rosplan_dispatch_msgs::ActionDispatch::ConstPtr& msg);
+	};
+}
+#endif
---
+++ rosplan_interface_movebase/src/RPMoveBase.cpp
@@ -0,0 +1,123 @@
+#include <ros/ros.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <boost/foreach.hpp>
+#include <actionlib/client/simple_action_client.h>
+#include "rosplan_dispatch_msgs/ActionDispatch.h"
+#include "rosplan_dispatch_msgs/ActionFeedback.h"
+#include "move_base_msgs/MoveBaseAction.h"
+#include "mongodb_store/message_store.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "rosplan_interface_movebase/RPMoveBase.h"
+
+/* The implementation of RPMoveBase.h */
+namespace KCL_rosplan {
+
+	/* constructor */
+	RPMoveBase::RPMoveBase(ros::NodeHandle &nh, std::string &actionserver)
+	 : message_store(nh), action_client(actionserver, true) {
+		
+		// create the action client
+		ROS_INFO("KCL: (MoveBase) waiting for action server to start on %s", actionserver.c_str());
+		action_client.waitForServer();
+		
+		// create the action feedback publisher
+		action_feedback_pub = nh.advertise<rosplan_dispatch_msgs::ActionFeedback>("/kcl_rosplan/action_feedback", 10, true);
+	}
+
+	/* action dispatch callback */
+	void RPMoveBase::dispatchCallback(const rosplan_dispatch_msgs::ActionDispatch::ConstPtr& msg) {
+
+		// ignore non-goto-waypoint actions
+		if(0!=msg->name.compare("goto_waypoint")) return;
+
+		ROS_INFO("KCL: (MoveBase) action recieved");
+
+		// get waypoint ID from action dispatch
+		std::string wpID;
+		bool found = false;
+		for(size_t i=0; i<msg->parameters.size(); i++) {
+			if(0==msg->parameters[i].key.compare("to")) {
+				wpID = msg->parameters[i].value;
+				found = true;
+			}
+		}
+		if(!found) {
+			ROS_INFO("KCL: (MoveBase) aborting action dispatch; malformed parameters");
+			return;
+		}
+		
+		// get pose from message store
+		std::vector< boost::shared_ptr<geometry_msgs::PoseStamped> > results;
+		if(message_store.queryNamed<geometry_msgs::PoseStamped>(wpID, results)) {
+			if(results.size()<1) {
+				ROS_INFO("KCL: (MoveBase) aborting action dispatch; no matching wpID %s", wpID.c_str());
+				return;
+			}
+			if(results.size()>1)
+				ROS_INFO("KCL: (MoveBase) multiple waypoints share the same wpID");
+
+			// dispatch MoveBase action
+			move_base_msgs::MoveBaseGoal goal;
+			geometry_msgs::PoseStamped &pose = *results[0];
+			goal.target_pose = pose;
+			goal.target_pose.pose.orientation.w = 1;
+			action_client.sendGoal(goal);
+
+			// publish feedback (enabled)
+			 rosplan_dispatch_msgs::ActionFeedback fb;
+			fb.action_id = msg->action_id;
+			fb.status = "action enabled";
+			action_feedback_pub.publish(fb);
+
+			bool finished_before_timeout = action_client.waitForResult(ros::Duration(msg->duration));
+			if (finished_before_timeout) {
+
+				actionlib::SimpleClientGoalState state = action_client.getState();
+				ROS_INFO("KCL: (MoveBase) action finished: %s", state.toString().c_str());
+				
+				// publish feedback (achieved)
+				 rosplan_dispatch_msgs::ActionFeedback fb;
+				fb.action_id = msg->action_id;
+				fb.status = "action achieved";
+				action_feedback_pub.publish(fb);
+
+			} else {
+
+				// publish feedback (failed)
+				 rosplan_dispatch_msgs::ActionFeedback fb;
+				fb.action_id = msg->action_id;
+				fb.status = "action failed";
+				action_feedback_pub.publish(fb);
+
+				ROS_INFO("KCL: (MoveBase) action timed out");
+
+			}
+
+		} else ROS_INFO("KCL: (MoveBase) aborting action dispatch; query to sceneDB failed");
+	}
+} // close namespace
+
+	/*-------------*/
+	/* Main method */
+	/*-------------*/
+
+	int main(int argc, char **argv) {
+
+		ros::init(argc, argv, "rosplan_interface_movebase");
+		ros::NodeHandle nh;
+
+		std::string actionserver;
+		nh.param("action_server", actionserver, std::string("/move_base"));
+
+		// create PDDL action subscriber
+		KCL_rosplan::RPMoveBase rpmb(nh, actionserver);
+	
+		// listen for action dispatch
+		ros::Subscriber ds = nh.subscribe("/kcl_rosplan/action_dispatch", 1000, &KCL_rosplan::RPMoveBase::dispatchCallback, &rpmb);
+		ROS_INFO("KCL: (MoveBase) Ready to receive");
+
+		ros::spin();
+		return 0;
+	}
