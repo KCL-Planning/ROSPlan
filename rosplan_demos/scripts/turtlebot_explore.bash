781c5d361ae17f61e2af3842876bd18ed392f55b
Michael Cashmore <michael.cashmore@kcl.ac.uk>
2015-06-28 19:53:49 +0100

    Update turtlebot_explore.bash

--- rosplan_demos/scripts/turtlebot_explore.bash
+++ rosplan_demos/scripts/turtlebot_explore.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 rosservice call /kcl_rosplan/roadmap_server/create_prm "{nr_waypoints: 10, min_distance: 0.3, casting_distance: 2.0, connecting_distance: 8.0, occupancy_threshold: 10, total_attempts: 10}";
+rosservice call /kcl_rosplan/update_knowledge_base "update_type: 1
+knowledge:
+  knowledge_type: 0
+  instance_type: 'robot'
+  instance_name: 'kenny'
+  attribute_name: ''
+  function_value: 0.0";
 for i in 0 1 2 3 4 5 6 7 8 9
 do
 rosservice call /kcl_rosplan/update_knowledge_base "update_type: 1
