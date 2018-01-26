5f14aa7aad5d12eee089f98bfff3b285e835209b
m312z <michael.cashmore@kcl.ac.uk>
2018-01-26 14:27:17 +0000

    cleaned up more launch files

--- rosplan_demos/scripts/simulated_actions.bash
+++ rosplan_demos/scripts/simulated_actions.bash
@@ -1,30 +1,26 @@
 #!/bin/bash
-rosservice call /kcl_rosplan/update_knowledge_base "update_type: 0
-knowledge:
-  knowledge_type: 0
+param_goals="knowledge:";
+param="knowledge:
+- knowledge_type: 0
   instance_type: 'robot'
   instance_name: 'kenny'
   attribute_name: ''
-  function_value: 0.0";
-rosservice call /kcl_rosplan/update_knowledge_base "update_type: 0
-knowledge:
-  knowledge_type: 0
+  function_value: 0.0
+- knowledge_type: 0
   instance_type: 'robot'
   instance_name: 'beth'
   attribute_name: ''
   function_value: 0.0";
 for i in $(seq 0 4 )
 do
-rosservice call /kcl_rosplan/update_knowledge_base "update_type: 0
-knowledge:
-  knowledge_type: 0
+param="$param
+- knowledge_type: 0
   instance_type: 'waypoint'
   instance_name: 'wp$i'
   attribute_name: ''
   function_value: 0.0";
-rosservice call /kcl_rosplan/update_knowledge_base "update_type: 1
-knowledge:
-  knowledge_type: 1
+param_goals="$param_goals
+- knowledge_type: 1
   instance_type: ''
   instance_name: ''
   attribute_name: 'visited'
@@ -32,71 +28,64 @@ knowledge:
   - {key: 'wp', value: 'wp$i'}
   function_value: 0.0"
 done;
-rosservice call /kcl_rosplan/update_knowledge_base "update_type: 1
-knowledge:
-  knowledge_type: 1
+param_goals="$param_goals
+- knowledge_type: 1
   instance_type: ''
   instance_name: ''
   attribute_name: 'docked'
   values:
   - {key: 'v', value: 'kenny'}
-  function_value: 0.0"
-rosservice call /kcl_rosplan/update_knowledge_base "update_type: 1
-knowledge:
-  knowledge_type: 1
+  function_value: 0.0
+- knowledge_type: 1
   instance_type: ''
   instance_name: ''
   attribute_name: 'docked'
   values:
   - {key: 'v', value: 'beth'}
   function_value: 0.0"
-rosservice call /kcl_rosplan/update_knowledge_base "update_type: 0
-knowledge:
-  knowledge_type: 1
+param="$param
+- knowledge_type: 1
   instance_type: ''
   instance_name: ''
   attribute_name: 'robot_at'
   values:
   - {key: 'v', value: 'kenny'}
   - {key: 'wp', value: 'wp0'}
-  function_value: 0.0";
-rosservice call /kcl_rosplan/update_knowledge_base "update_type: 0
-knowledge:
-  knowledge_type: 1
+  function_value: 0.0
+- knowledge_type: 1
   instance_type: ''
   instance_name: ''
   attribute_name: 'robot_at'
   values:
   - {key: 'v', value: 'beth'}
   - {key: 'wp', value: 'wp0'}
-  function_value: 0.0";
-rosservice call /kcl_rosplan/update_knowledge_base "update_type: 0
-knowledge:
-  knowledge_type: 1
+  function_value: 0.0
+- knowledge_type: 1
   instance_type: ''
   instance_name: ''
   attribute_name: 'docked'
   values:
   - {key: 'v', value: 'kenny'}
-  function_value: 0.0";
-rosservice call /kcl_rosplan/update_knowledge_base "update_type: 0
-knowledge:
-  knowledge_type: 1
+  function_value: 0.0
+- knowledge_type: 1
   instance_type: ''
   instance_name: ''
   attribute_name: 'docked'
   values:
   - {key: 'v', value: 'beth'}
-  function_value: 0.0";
-rosservice call /kcl_rosplan/update_knowledge_base "update_type: 0
-knowledge:
-  knowledge_type: 1
+  function_value: 0.0
+- knowledge_type: 1
   instance_type: ''
   instance_name: ''
   attribute_name: 'dock_at'
   values:
   - {key: 'wp', value: 'wp0'}
   function_value: 0.0";
+
+rosservice call /kcl_rosplan/update_knowledge_base_array "update_type: 0
+$param"
+rosservice call /kcl_rosplan/update_knowledge_base_array "update_type: 1
+$param_goals"
 rosservice call /rosplan_problem_interface/problem_generation_server;
 rosservice call /rosplan_planner_interface/planning_server;
 rosservice call /rosplan_parsing_interface/parse_plan;
