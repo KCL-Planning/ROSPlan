3a21b503328833f69b41d2a9844884c32f96e997
Gerard Canal <grar.knal@gmail.com>
2018-09-18 20:16:16 +0100

    Added RDDLTask parser interface

---
+++ rosplan_knowledge_base/src/RDDLTaskParser.cpp
@@ -0,0 +1,41 @@
+
+#include <rosplan_knowledge_base/RDDLTaskParser.h>
+
+#include "rosplan_knowledge_base/RDDLTaskParser.h"
+
+/* implementation of RDDLTaskParser.h */
+namespace KCL_rosplan {
+
+	/*----------------*/
+	/* parsing domain */
+	/*----------------*/
+
+	/**
+	 * parse the domain file
+	 */
+    RDDLTask *RDDLTaskParser::parseTask(const std::string &domainPath, const std::string &instancePath) {
+		// only parse domain once
+		if (domain_parsed) return rddlTask;
+        ROS_INFO("KCL: (%s) Parsing domain: %s.", ros::this_node::getName().c_str(), domainPath.c_str());
+
+        // parse domain
+        rddlTask = RDDLParser::parseRDDLTask(domainPath, instancePath);
+        if (not rddlTask) {
+            ROS_ERROR("KCL: (%s) Failed to open domain file.", ros::this_node::getName().c_str());
+            return nullptr;
+        }
+        domain_name = rddlTask->domainName;
+        problem_name = rddlTask->name;
+        domain_parsed = true;
+
+		return rddlTask;
+	}
+
+} // close namespace
+
+
+int main() {
+    KCL_rosplan::RDDLTaskParser dp;
+    dp.parseTask("/home/gerard/code/ROSPlan_ws/src/rosplan/rosplan_demos/common/turtlebot_domain.rddl",
+                 "/home/gerard/code/ROSPlan_ws/src/rosplan/rosplan_demos/common/turtlebot_inst.rddl");
+}
