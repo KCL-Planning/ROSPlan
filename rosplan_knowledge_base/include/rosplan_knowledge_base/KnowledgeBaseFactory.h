02660636d1068ba7f8b4d7ecf16ecd6b900636db
Gerard Canal <grar.knal@gmail.com>
2018-09-24 15:35:16 +0100

    Added KnowledgeBase Factory

---
+++ rosplan_knowledge_base/include/rosplan_knowledge_base/KnowledgeBaseFactory.h
@@ -0,0 +1,26 @@
+//
+// Created by Gerard Canal on 24/09/18.
+//
+
+#ifndef ROSPLAN_KNOWLEDGE_BASE_KNOWLEDGEBASEFACTORY_H
+#define ROSPLAN_KNOWLEDGE_BASE_KNOWLEDGEBASEFACTORY_H
+
+#include "rosplan_knowledge_base/KnowledgeBase.h"
+#include "rosplan_knowledge_base/PDDLKnowledgeBase.h"
+#include "rosplan_knowledge_base/RDDLKnowledgeBase.h"
+
+namespace KCL_rosplan {
+    typedef std::unique_ptr<KnowledgeBase> KnowledgeBasePtr;
+
+    class KnowledgeBaseFactory {
+    public:
+        enum KB {
+            PDDL,
+            RDDL
+        };
+
+        static KnowledgeBasePtr createKB(KB kb_type);
+    };
+}
+
+#endif //ROSPLAN_KNOWLEDGE_BASE_KNOWLEDGEBASEFACTORY_H
