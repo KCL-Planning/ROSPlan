0a020e61855ceec89abdf99caae4f125bb47e23f
Oscar Lima <olima_84@yahoo.com>
2019-01-28 20:32:19 +0000

    save progress, add all required functions, set todos

--- rosplan_planning_system/include/rosplan_planning_system/PlanDispatch/CSPExecGenerator.h
+++ rosplan_planning_system/include/rosplan_planning_system/PlanDispatch/CSPExecGenerator.h
@@ -22,6 +22,7 @@
 #include <diagnostic_msgs/KeyValue.h>
 #include <std_msgs/String.h>
 #include <std_srvs/SetBool.h>
+// #include <rosplan_dispatch_msgs/ExecAlternatives.h>
 #include "rosplan_action_interface/ActionSimulator.h"
 
 class CSPExecGenerator
@@ -45,17 +46,45 @@ class CSPExecGenerator
         void esterelPlanCB(const rosplan_dispatch_msgs::EsterelPlan::ConstPtr& msg);
 
         /**
-         * @brief remove conditional edges from a fully connected esterel plan received in original_fully_ordered_plan_
-         * @param plan the plan from which the conditional edges need to be removed
-         * @return a partially ordered plan with only interference edges in EsterelPlan format
+         * @brief init C : set of temporal constraints
+         * @param set_of_constraints return value gets written here by reference
          */
-        rosplan_dispatch_msgs::EsterelPlan removeConditionalEdges(rosplan_dispatch_msgs::EsterelPlan plan);
+        void initConstraints(std::map<int, int> &set_of_constraints);
+
+        /**
+         * @brief check if set of ordered nodes satisfies the set of constraints
+         * @param ordered_nodes F or set of ordered nodes
+         * @param set_of_constraints C or set of constraints
+         * @return true if the set of nodes satisfy the set of constraints, false otherwise
+         */
+        bool checkTemporalConstraints(std::vector<int> &set_of_ordered_nodes,
+            std::map<int, int> &set_of_constraints);
+
+        /**
+         * @brief check if curent state (P) satisfies all goals proposed in (G)
+         * @param as action simulator object, it contains a method to check if goals ae achieved
+         */
+        bool areGoalsAchieved(ActionSimulator &as);
+
+        /**
+         * @brief iterate over open list (O), check if node preconditions are met in current state (P)
+         * @param open_list set of unordered nodes
+         * @return set of nodes (V) which preconditions are met in current state (P)
+         */
+        std::vector<rosplan_dispatch_msgs::EsterelPlanNode> validNodes(
+            std::vector<rosplan_dispatch_msgs::EsterelPlanNode> open_list);
+
+        /**
+         * @brief shift nodes from open list (O) to ordered plans (R) offering different execution alternatives
+         * @return true if at least one valid execution was found, false otherwise
+         */
+        bool orderNodes();
 
         /**
          * @brief generate plan alternatives based on search
          * @return true if succeeded
          */
-        bool computeExecAlternatives();
+        bool generateFullyConnectedPlan();
 
         /**
          * @brief service callback with user request to generate execution alternatives
@@ -64,27 +93,39 @@ class CSPExecGenerator
          * one possible execution was found, false if replan is needed (means plan is invalid)
          * @return true if service finished execution, false otherwise
          */
-        bool srvCB(std_srvs::SetBool::Response &req, std_srvs::SetBool::Response &res);
+        bool srvCB(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
+
+        /**
+         * @brief wait for callbacks to arrive
+         */
+        void update();
 
     private:
         // ros related variables
         ros::NodeHandle nh_;
         ros::Publisher pub_set_of_solutions_, pub_pop_;
         ros::Subscriber sub_esterel_plan_;
+        ros::ServiceServer srv_gen_alternatives_;
 
         /// flag used to know when we have received a callback
         bool is_esterel_plan_received_;
 
         /// stores the received msg in esterel plan callback
-        rosplan_dispatch_msgs::EsterelPlan original_fully_ordered_plan_;
+        rosplan_dispatch_msgs::EsterelPlan original_plan_;
 
-        /// to store the partial ordered plan, only with interference edges
-        rosplan_dispatch_msgs::EsterelPlan partial_order_plan_;
+        /// P: to simulate actions in a private (own) KB
+        ActionSimulator *action_simulator_;
 
-        /// plan execution alternatives, each one is a fully ordered esterel plan
-        rosplan_dispatch_msgs::EsterelPlanArray solution_set_;
+        /// O: open list, the list of nodes id's to order
+        std::vector<int> open_list_;
 
-        /// to simulate actions in a private (own) KB
-        ActionSimulator action_simulator_;
+        /// C: set of constraints
+        std::map<int, int> set_of_constraints_;
+
+        // F: store the set of ordered nodes id's, empty at the beginning
+        std::vector<int> ordered_nodes_;
+
+        /// R: plan execution alternatives, each one is a fully ordered esterel plan
+        rosplan_dispatch_msgs::EsterelPlanArray ordered_plans_;
 };
 #endif  // CSP_EXEC_GENERATOR_NODE_H
--- rosplan_planning_system/src/PlanDispatch/CSPExecGenerator.cpp
+++ rosplan_planning_system/src/PlanDispatch/CSPExecGenerator.cpp
@@ -15,21 +15,27 @@
 CSPExecGenerator::CSPExecGenerator() : nh_("~"), is_esterel_plan_received_(false)
 {
     // subscriptions: subscribe to esterel plan, a fully ordered plan
-    sub_esterel_plan_ = nh_.subscribe("/rosplan_parsing_interface/complete_plan", 1, &CSPExecGenerator::esterelPlanCB, this);
+    sub_esterel_plan_ = nh_.subscribe("/rosplan_parsing_interface/complete_plan2", 1, &CSPExecGenerator::esterelPlanCB, this);
 
     // publications
     // array of esterel plans with different execution alternatives
-    pub_set_of_solutions_ = nh_.advertise<rosplan_dispatch_msgs::EsterelPlanArray>("~set_of_solutions", 1);
+    pub_set_of_solutions_ = nh_.advertise<rosplan_dispatch_msgs::EsterelPlanArray>("set_of_solutions", 1);
     // partially ordered plan = original plan - conditional edges = plan with only interference edges
-    pub_pop_ = nh_.advertise<rosplan_dispatch_msgs::EsterelPlan>("~partially_ordered_plan", 1);
+    pub_pop_ = nh_.advertise<rosplan_dispatch_msgs::EsterelPlan>("/rosplan_parsing_interface/complete_plan", 1);
 
     // services: compute different execution alternatives from a partially ordered esterel plan (a plan
     // with no conditional edges, but only interference edges)
-    ros::ServiceServer service = nh_.advertiseService("gen_exec_alternatives", &CSPExecGenerator::srvCB, this);
+    srv_gen_alternatives_ = nh_.advertiseService("gen_exec_alternatives", &CSPExecGenerator::srvCB, this);
+
+    // call constructor, mirror KB (query real KB and get its data) but without facts and goals
+    action_simulator_ = new ActionSimulator(true, false);
 }
 
 CSPExecGenerator::~CSPExecGenerator()
 {
+    // delete pointer
+    delete action_simulator_;
+
     // shut down publishers and subscribers
     sub_esterel_plan_.shutdown();
     pub_set_of_solutions_.shutdown();
@@ -38,75 +44,107 @@ CSPExecGenerator::~CSPExecGenerator()
 
 void CSPExecGenerator::esterelPlanCB(const rosplan_dispatch_msgs::EsterelPlan::ConstPtr& msg)
 {
-    original_fully_ordered_plan_ = *msg;
+    original_plan_ = *msg;
     is_esterel_plan_received_ = true;
 }
 
-rosplan_dispatch_msgs::EsterelPlan CSPExecGenerator::removeConditionalEdges(rosplan_dispatch_msgs::EsterelPlan plan)
+void CSPExecGenerator::initConstraints(std::map<int, int> &set_of_constraints)
 {
-    // remove conditional edges from a fully connected esterel plan received in original_fully_ordered_plan_
+    // construct set of contraints from original received esterel plan
+    // example:
+    // C : [2:3, 5:6]; node 3 goes after node 2, node 6 goes after node 5
 
-    // iterate over edges
-    for(auto it=original_fully_ordered_plan_.edges.begin(); it!=original_fully_ordered_plan_.edges.end(); it++) {
-        // discriminate by conditional edges
-        if(it->edge_type == rosplan_dispatch_msgs::EsterelPlanEdge::CONDITION_EDGE)
-            // delete elements which match criteria
-            original_fully_ordered_plan_.edges.erase(it);
+    // delete previous data if any
+    set_of_constraints.clear();
+
+    // init set of temporal constraints (C)
+    for(auto eit=original_plan_.edges.begin(); eit!=original_plan_.edges.begin(); eit++) { // eit = edge iterator
+        // discriminate for interference edges
+        if(eit->edge_type == rosplan_dispatch_msgs::EsterelPlanEdge::INTERFERENCE_EDGE ||
+            eit->edge_type == rosplan_dispatch_msgs::EsterelPlanEdge::START_END_ACTION_EDGE) {
+            // add constraint
+            set_of_constraints.insert(std::pair<int, int>(eit->source_ids[0], eit->sink_ids[0]));
+            continue;
+        }
+        // NOTE: condition edges are ignored: we consider only a partially orderes plan
     }
-
-    // publish the partially ordered plan, previous plan without conditional edges
-    pub_pop_.publish(original_fully_ordered_plan_);
 }
 
-bool CSPExecGenerator::computeExecAlternatives()
+bool CSPExecGenerator::checkTemporalConstraints(std::vector<int> &set_of_ordered_nodes,
+    std::map<int, int> &set_of_constraints)
 {
-    // remove conditional edges from plan
-    partial_order_plan_ = removeConditionalEdges(original_fully_ordered_plan_);
+    // check if set of ordered nodes (F) satisfies the set of constraints (C)
 
-    rosplan_dispatch_msgs::EsterelPlan valid_execution_alternative;
+    // TODO
 
-    rosplan_dispatch_msgs::EsterelPlanNode node;
-    node.node_type = rosplan_dispatch_msgs::EsterelPlanNode::ACTION_START;
-    // node.node_type = rosplan_dispatch_msgs::EsterelPlanNode::ACTION_END;
-    // node.node_type = rosplan_dispatch_msgs::EsterelPlanNode::PLAN_START;
-    node.node_id = 0; // integer
-    node.name = ""; // str
-        rosplan_dispatch_msgs::ActionDispatch action;
-        action.action_id = 0; // int
-        action.name = ""; // str
-            diagnostic_msgs::KeyValue param;
-            param.key = ""; // str
-            param.value = ""; // str
-        action.parameters.push_back(param);
-        action.duration = 0.0; //float
-        action.dispatch_time = 0.0; // float
-    node.action = action;
-    node.edges_out.push_back(0); // int array
-    node.edges_in.push_back(0); // int array
-
-    rosplan_dispatch_msgs::EsterelPlanEdge edge;
-    edge.edge_type = rosplan_dispatch_msgs::EsterelPlanEdge::CONDITION_EDGE;
-    // edge.edge_type = rosplan_dispatch_msgs::EsterelPlanEdge::START_END_ACTION_EDGE;
-    // edge.edge_type = rosplan_dispatch_msgs::EsterelPlanEdge::INTERFERENCE_EDGE;
-    edge.edge_id = 0; // int
-    edge.edge_name = "";
-    edge.signal_type = 0; // int
-    edge.source_ids.push_back(0); // int array
-    edge.sink_ids.push_back(0); // int
-    edge.duration_lower_bound = 0.0; // float
-    edge.duration_upper_bound = 0.0; // float
-
-    valid_execution_alternative.nodes.push_back(node);
-    valid_execution_alternative.edges.push_back(edge);
-
-    // add plan to the set
-    solution_set_.esterel_plans.push_back(valid_execution_alternative);
-
-    // if true, it means at least one valid execution alternative was found
     return true;
 }
 
-bool CSPExecGenerator::srvCB(std_srvs::SetBool::Response &req, std_srvs::SetBool::Response &res)
+bool CSPExecGenerator::areGoalsAchieved(ActionSimulator &as)
+{
+    // check if curent state (P) satisfies all goals proposed in (G)
+
+    if(as.areGoalsAchieved()) {
+        // add new valid ordering to ordered plans (R)
+        // TODO
+
+        // return success
+        return true;
+    }
+
+    // goals are not achieved yet
+    return false;
+}
+
+std::vector<rosplan_dispatch_msgs::EsterelPlanNode> CSPExecGenerator::validNodes(
+    std::vector<rosplan_dispatch_msgs::EsterelPlanNode> open_list)
+{
+    // iterate over open list (O), check if node preconditions are met in current state (P)
+
+    std::vector<rosplan_dispatch_msgs::EsterelPlanNode> valid_nodes;
+
+    // TODO: complete
+    rosplan_dispatch_msgs::EsterelPlanNode node;
+    valid_nodes.push_back(node);
+
+    return valid_nodes;
+}
+
+bool CSPExecGenerator::orderNodes()
+{
+    // shift nodes from open list (O) to ordered plans (R) offering different execution alternatives
+    if(!checkTemporalConstraints(ordered_nodes_, set_of_constraints_))
+        return false;
+
+    // TODO: largely unfinished
+
+    return true;
+}
+
+bool CSPExecGenerator::generateFullyConnectedPlan()
+{
+    // get current state (P) and store in memory
+    action_simulator_->saveKBSnapshot();
+
+    // init open list (O), initially contains all nodes in partial order plan
+    open_list_.clear();
+    for(auto nit=original_plan_.nodes.begin(); nit!=original_plan_.nodes.end(); nit++) // nit=node iterator
+        open_list_.push_back(nit->node_id);
+
+    // init set of constraints (C)
+    initConstraints(set_of_constraints_);
+
+    // init set of ordered nodes (F)
+    ordered_nodes_.clear();
+
+    // init set of totally ordered plans (R)
+    ordered_plans_.esterel_plans.clear();
+
+    // if true, it means at least one valid execution alternative was found
+    return orderNodes();
+}
+
+bool CSPExecGenerator::srvCB(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& res)
 {
     ROS_INFO("Generating execution alternatives service is computing now");
 
@@ -120,14 +158,14 @@ bool CSPExecGenerator::srvCB(std_srvs::SetBool::Response &req, std_srvs::SetBool
         return true;
     }
 
-    if(computeExecAlternatives())
+    if(generateFullyConnectedPlan())
     {
         // indicates that at least one valid execution was found
         res.success = true; // set result of srv as success
         ROS_INFO("Found valid execution(s)");
 
         // publish esterel graph array (multiple ways of executing plan)
-        pub_set_of_solutions_.publish(solution_set_);
+        pub_set_of_solutions_.publish(ordered_plans_);
     }
     else
     {
@@ -144,6 +182,12 @@ bool CSPExecGenerator::srvCB(std_srvs::SetBool::Response &req, std_srvs::SetBool
     return true;
 }
 
+void CSPExecGenerator::update()
+{
+    // check if callbacks have requests
+    ros::spinOnce();
+}
+
 int main(int argc, char **argv)
 {
     // init node
@@ -152,10 +196,24 @@ int main(int argc, char **argv)
 
     // create object of the node class (CSPExecGenerator)
     CSPExecGenerator csp_exec_generator_node;
+
+    // setup node frequency
+    double node_frequency = 1.0;
+    ros::NodeHandle nh("~");
+    nh.param("node_frequency", node_frequency, 1.0);
+    ROS_INFO("Node will run at : %lf [hz]", node_frequency);
+    ros::Rate loop_rate(node_frequency);
+
     ROS_INFO("Node initialized.");
 
-    // wait for callbacks (incoming topics or service calls
-    ros::spin();
+    while (ros::ok())
+    {
+        // main loop function
+        csp_exec_generator_node.update();
+
+        // sleep to control the node frequency
+        loop_rate.sleep();
+    }
 
     return 0;
 }
