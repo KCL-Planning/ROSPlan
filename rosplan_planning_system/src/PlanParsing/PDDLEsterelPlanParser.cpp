a25a8ab2fc4f0769d3c83cc6d3e10fccd8c27f4c
Michael Cashmore <michael.cashmore@kcl.ac.uk>
2019-02-03 10:31:36 +0000

    Update PDDLEsterelPlanParser.cpp

--- rosplan_planning_system/src/PlanParsing/PDDLEsterelPlanParser.cpp
+++ rosplan_planning_system/src/PlanParsing/PDDLEsterelPlanParser.cpp
@@ -470,7 +470,7 @@ namespace KCL_rosplan {
 		if(lower_bound == 0)
 			lower_bound = 0.001;
 		if(upper_bound == 0)
-			lower_bound = 0.001;
+			upper_bound = 0.001;
 		
 		// see if there is already an existing edge
 		std::vector<int>::iterator eit = last_plan.nodes[source_node_id].edges_out.begin();
