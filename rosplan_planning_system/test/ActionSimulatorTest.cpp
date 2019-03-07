fc9f95c9e3dad0b3a8840b100fcb6de3b2b76c3c
Oscar Lima <oscar.lima_carrion@dfki.de>
2019-03-07 21:27:24 +0100

    add test cases to action simulator

--- rosplan_planning_system/test/ActionSimulatorTest.cpp
+++ rosplan_planning_system/test/ActionSimulatorTest.cpp
@@ -16,159 +16,302 @@
 #include <rosplan_planning_system/PlanDispatch/CSPExecGenerator.h>
 #include <string>
 
-std::string event_out_response;
-bool callback_received = false;
+// declare object of the class ActionSimulator
+ActionSimulator *action_simulator_tester;
 
-// get operator names test
-TEST(action_simulator_test, success_test)
+bool compareVectors(std::vector<std::string> &vector1, std::vector<std::string> &vector2)
 {
-    // create object of the node class (ActionSimulator)
-    ActionSimulator action_simulator_tester(true, true);
+    // iterate over vector 1, see if all of its elements can be found in vector 2
 
+    // init response to true, might get modified below
+    bool vectors_are_equivalent = true;
+
+    // iterate over vector1 and find its elements in vector2
+    for(auto vit=vector1.begin(); vit!=vector1.end(); vit++) {
+        if(!(std::find(vector2.begin(), vector2.end(), *vit) != vector2.end())) {
+            // element not found
+            vectors_are_equivalent = false;
+            ROS_ERROR("element (%s) missing", vit->c_str());
+        }
+    }
+
+    return vectors_are_equivalent;
+}
+
+TEST(action_simulator_test, get_operator_names)
+{
     std::vector<std::string> operator_names;
-    action_simulator_tester.getOperatorNames(operator_names);
+    action_simulator_tester->getOperatorNames(operator_names);
     ROS_INFO("get operator names test");
 
     std::vector<std::string> expected_operator_names = {"goto_waypoint", "localise", "dock", "undock", \
         "wait_load_at_machine", "switch_machine_on", "wait_unload", "ditch"};
 
-    bool vectors_are_equivalent = true;
+    ASSERT_EQ(compareVectors(operator_names, expected_operator_names), true);
+}
 
-    // iterate over operator_names vector and find its elements in operator_names
-    for(auto onit=expected_operator_names.begin(); onit!=expected_operator_names.end(); onit++) {
-        // find operator names in the fetched ones
-        if(!(std::find(operator_names.begin(), operator_names.end(), *onit) != operator_names.end())) {
-            // element not found
-            vectors_are_equivalent = false;
-            ROS_ERROR("operator name (%s) missing in domain", onit->c_str());
-        }
-    }
+TEST(action_simulator_test, get_domain_predicates)
+{
+    std::vector<std::string> domain_predicates;
+    action_simulator_tester->getAllPredicateNames(domain_predicates);
+    ROS_INFO("get all predicate names test");
 
-    ASSERT_EQ(vectors_are_equivalent, true);
+    std::vector<std::string> expected_domain_predicates = {"robot_at", "undocked", "docked", "localised", \
+        "dock_at", "carrying_order", "nocarrying_order", "asked_unload", "order_delivered", \
+        "delivery_destination", "machine_on", "machine_off"};
+
+    ASSERT_EQ(compareVectors(domain_predicates, expected_domain_predicates), true);
+}
+
+TEST(action_simulator_test, save_kb_snapshot_and_find_fact_and_find_goal)
+{
+    ROS_INFO("print KB test");
+
+    action_simulator_tester->saveKBSnapshot();
+    action_simulator_tester->printInternalKBFacts();
+    action_simulator_tester->printInternalKBGoals();
+
+    std::string action;
+    std::vector<std::string> params;
+
+    action = "robot_at";
+    params = {"robot0", "wp0"};
+    ASSERT_EQ(action_simulator_tester->findFactInternal(action, params), true);
+
+    action = "nocarrying_order";
+    params = {"robot0"};
+    ASSERT_EQ(action_simulator_tester->findFactInternal(action, params), true);
+
+    action = "undocked";
+    params = {"robot0"};
+    ASSERT_EQ(action_simulator_tester->findFactInternal(action, params), true);
+
+    action = "localised";
+    ASSERT_EQ(action_simulator_tester->findFactInternal(action, params), true);
+
+    action = "robot_at";
+    params = {"robot1", "wp0"};
+    ASSERT_EQ(action_simulator_tester->findFactInternal(action, params), true);
+
+    action = "nocarrying_order";
+    params = {"robot1"};
+    ASSERT_EQ(action_simulator_tester->findFactInternal(action, params), true);
+
+    action = "undocked";
+    params = {"robot1"};
+    ASSERT_EQ(action_simulator_tester->findFactInternal(action, params), true);
+
+    action = "localised";
+    params = {"robot1"};
+    ASSERT_EQ(action_simulator_tester->findFactInternal(action, params), true);
+
+    /* we do not check for this facts:
+    (robot_at robot2 wp0)
+    (nocarrying_order robot2)
+    (undocked robot2)
+    (localised robot2)
+    (machine_off machine0)
+    (machine_off machine1)
+    (machine_off machine2)
+    (machine_off machine3)
+    (machine_off machine4)
+    (delivery_destination wp4)
+    (delivery_destination wp2)
+    (delivery_destination wp3)*/
+
+    // find goals
+    std::string goal= "order_delivered";
+
+    params = {"wp4"};
+    ASSERT_EQ(action_simulator_tester->findGoalInternal(goal, params), true);
+
+    params = {"wp2"};
+    ASSERT_EQ(action_simulator_tester->findGoalInternal(goal, params), true);
+
+    params = {"wp3"};
+    ASSERT_EQ(action_simulator_tester->findGoalInternal(goal, params), true);
+}
+
+TEST(action_simulator_test, remove_predicate_from_kb)
+{
+    std::string action = "localised";
+    std::vector<std::string> params = {"robot1"};
+    action_simulator_tester->removeFactInternal(action, params);
+    ASSERT_EQ(action_simulator_tester->findFactInternal(action, params), false);
+}
+
+TEST(action_simulator_test, action_applicability)
+{
+    // action is applicable
+    std::string action = "goto_waypoint";
+    std::vector<std::string> params = {"robot0", "wp0", "wp1"};
+    ASSERT_EQ(action_simulator_tester->isActionApplicable(action, params), true);
+
+    // action not applicable
+    action = "wait_load_at_machine";
+    params = {"robot0", "robot1", "machine2"};
+    ASSERT_EQ(action_simulator_tester->isActionApplicable(action, params), false);
+}
+
+TEST(action_simulator_test, simulate_action)
+{
+    // action is applicable
+    std::string action = "goto_waypoint";
+    std::vector<std::string> params = {"robot0", "wp0", "wp1"};
+
+    // simulate action start
+    action_simulator_tester->simulateActionStart(action, params);
+
+    // check effects
+    // robot not at wp0 anymore
+    std::string predicate_name = "robot_at";
+    params = {"robot0", "wp0"};
+    ASSERT_EQ(action_simulator_tester->findFactInternal(predicate_name, params), false);
+
+    predicate_name = "asked_unload";
+    params = {"robot0"};
+    ASSERT_EQ(action_simulator_tester->findFactInternal(predicate_name, params), false);
+
+    // robot at destination (mm not yet, this is at end effect)
+    predicate_name = "robot_at";
+    params = {"robot0", "wp1"};
+    ASSERT_EQ(action_simulator_tester->findFactInternal(predicate_name, params), false);
+
+    // simulate action end
+    action = "goto_waypoint";
+    params = {"robot0", "wp0", "wp1"};
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    // robot at destination
+    predicate_name = "robot_at";
+    params = {"robot0", "wp1"};
+    ASSERT_EQ(action_simulator_tester->findFactInternal(predicate_name, params), true);
+}
+
+TEST(action_simulator_test, goals_achieved)
+{
+    // reset internal kb
+    action_simulator_tester->saveKBSnapshot();
+
+    // goals should be false at this point
+    ASSERT_EQ(action_simulator_tester->areGoalsAchieved(), false);
+
+    std::string action;
+    std::vector<std::string> params;
+
+    // simulate entire plan
+    action = "goto_waypoint";
+    params = {"robot0", "wp0", "machine4"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "switch_machine_on";
+    params = {"robot0", "machine4"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "goto_waypoint";
+    params = {"robot2", "wp0", "machine4"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "wait_load_at_machine";
+    params = {"robot0", "robot2", "machine4"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "goto_waypoint";
+    params = {"robot0", "machine4", "wp3"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "wait_unload";
+    params = {"robot0", "wp3"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "goto_waypoint";
+    params = {"robot0", "wp3", "machine4"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "wait_load_at_machine";
+    params = {"robot0", "robot2", "machine4"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "goto_waypoint";
+    params = {"robot0", "machine4", "wp2"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "wait_unload";
+    params = {"robot0", "wp2"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "goto_waypoint";
+    params = {"robot1", "wp0", "machine4"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "wait_load_at_machine";
+    params = {"robot1", "robot2", "machine4"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "goto_waypoint";
+    params = {"robot1", "machine4", "wp4"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    action = "wait_unload";
+    params = {"robot1", "wp4"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    // goal should be achieved now! after plan execution
+    ASSERT_EQ(action_simulator_tester->areGoalsAchieved(), true);
+}
+
+TEST(action_simulator_test, revert_action)
+{
+    // reset internal kb
+    action_simulator_tester->saveKBSnapshot();
+
+    // simulate action
+    std::string action = "goto_waypoint";
+    std::vector<std::string> params = {"robot0", "wp0", "wp1"};
+    action_simulator_tester->simulateActionStart(action, params);
+    action_simulator_tester->simulateActionEnd(action, params);
+
+    // revert action
+    action_simulator_tester->revertActionStart(action, params);
+    action_simulator_tester->revertActionEnd(action, params);
+
+    std::string predicate_name;
+
+    // check robot should be at same location as it was
+    predicate_name = "robot_at";
+    params = {"robot0", "wp0"};
+    // should be there
+    ASSERT_EQ(action_simulator_tester->findFactInternal(predicate_name, params), true);
+
+    predicate_name = "robot_at";
+    params = {"robot0", "wp1"};
+    // should not be there
+    ASSERT_EQ(action_simulator_tester->findFactInternal(predicate_name, params), false);
+
+    predicate_name = "asked_unload";
+    params = {"robot0"};
+    // should not be there
+    ASSERT_EQ(action_simulator_tester->findFactInternal(predicate_name, params), false);
 }
 
 int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "action_simulator_test_node");
     testing::InitGoogleTest(&argc, argv);
-    // init node
-    ros::init(argc, argv, "action_simulator_test");
-    ROS_INFO("Test initialized");
-
+    action_simulator_tester = new ActionSimulator(true, true);
     return RUN_ALL_TESTS();
 }
-
-
-//
-//     // snippet: get all domain predicate names
-//     std::vector<std::string> domain_predicates;
-//     action_simulator_tester.getAllPredicateNames(domain_predicates);
-//     ROS_DEBUG("get all predicate names:");
-//     ROS_DEBUG("================");
-//     for(auto it=domain_predicates.begin(); it != domain_predicates.end(); it++)
-//     {
-//         ROS_DEBUG("%s", it->c_str());
-//     }
-//
-//     // save all predicates to internal KB
-//     action_simulator_tester.saveKBSnapshot();
-//
-//     // snippet: print internal KB facts
-//     ROS_DEBUG("print internal KB facts:");
-//     ROS_DEBUG("================");
-//     action_simulator_tester.printInternalKBFacts();
-//
-//     // snippet: print internal KB goals
-//     ROS_DEBUG("print internal KB goals:");
-//     ROS_DEBUG("================");
-//     action_simulator_tester.printInternalKBGoals();
-//
-//     // snippet: find fact in KB (with no args)
-//     ROS_DEBUG("find fact in internal KB no args:");
-//     ROS_DEBUG("================");
-//     std::string predicate_name2 = "person_descending";
-//     if(action_simulator_tester.findFactInternal(predicate_name2))
-//         ROS_DEBUG("predicate : (person_descending) found in internal KB");
-//     else
-//         ROS_DEBUG("predicate : (person_descending) not found in internal KB");
-//     // alternative option
-//     ROS_DEBUG("find fact in internal KB no args (alternative option):");
-//     ROS_DEBUG("================");
-//     std::string predicate_name3 = "person_descending";
-//     std::vector<std::string> args1 = {""};
-//     if(action_simulator_tester.findFactInternal(predicate_name3, args1))
-//         ROS_DEBUG("predicate : (person_descending) found in internal KB");
-//     else
-//         ROS_DEBUG("predicate : (person_descending) not found in internal KB");
-//
-//     // snippet: find fact in KB (with args)
-//     ROS_DEBUG("find fact in internal KB with args:");
-//     ROS_DEBUG("================");
-//     std::string predicate_name4 = "has_driver_license";
-//     std::vector<std::string> args2 = {"batdad"};
-//     if(action_simulator_tester.findFactInternal(predicate_name4, args2))
-//         ROS_DEBUG("predicate : (has_driver_license batdad) found in internal KB");
-//     else
-//         ROS_DEBUG("predicate : (has_driver_license batdad) not found in internal KB");
-//
-//     // snippet: remove predicate from KB (with args)
-//     std::string predicate_name = "has_driver_license";
-//     std::vector<std::string> args3 = {"batdad"};
-//     action_simulator_tester.removeFactInternal(predicate_name, args3);
-//     ROS_DEBUG("test: remove predicate with args (has_driver_license batdad):");
-//     ROS_DEBUG("================");
-//     action_simulator_tester.printInternalKBFacts();
-//
-//     // snippet: remove predicate from KB (no args)
-//     std::string predicate_name5 = "person_descending";
-//     std::vector<std::string> args4 = {""};
-//     ROS_DEBUG("test: remove predicate with no args (person_descending):");
-//     ROS_DEBUG("================");
-//     action_simulator_tester.removeFactInternal(predicate_name5, args4);
-//     action_simulator_tester.printInternalKBFacts();
-//
-//     // snippet: see if action is applicable
-//     std::string action_name = "get_down_from_car";
-//     std::vector<std::string> params = {"batdad","car","ben_school"}; // person, car, location
-//     ROS_DEBUG("check if action is applicable: (get_down_from_car batdad car ben_school), expected outcome is true");
-//     ROS_DEBUG("================");
-//     if(action_simulator_tester.isActionApplicable(action_name, params))
-//         ROS_DEBUG("action is applicable!");
-//     else
-//         ROS_DEBUG("action is not applicable");
-//
-//     // snippet: simulate an action start
-//     ROS_DEBUG("simulate action start: (get_down_from_car batdad car ben_school)");
-//     ROS_DEBUG("================");
-//     ROS_DEBUG("KB before simulation");
-//     action_simulator_tester.printInternalKBFacts();
-//     std::string action_name2 = "get_down_from_car";
-//     std::vector<std::string> params2 = {"batdad","car","ben_school"};
-//     action_simulator_tester.simulateActionStart(action_name2, params2);
-//     ROS_DEBUG("KB after simulation");
-//     action_simulator_tester.printInternalKBFacts();
-//
-//     // snippet: test areGoalsAchieved()
-//     ROS_DEBUG("Check if goals are achieved:");
-//     ROS_DEBUG("================");
-//     ROS_DEBUG("KB goals:");
-//     action_simulator_tester.printInternalKBGoals();
-//     ROS_DEBUG("KB facts:");
-//     action_simulator_tester.printInternalKBFacts();
-//     if(action_simulator_tester.areGoalsAchieved())
-//         ROS_DEBUG("goals achieved");
-//     else
-//         ROS_DEBUG("goals not achieved");
-//
-//     // snippet: test revert actions
-//     ROS_DEBUG("Check reverting action: (get_down_from_car batdad car ben_school)");
-//     ROS_DEBUG("================");
-//     ROS_DEBUG("KB facts:");
-//     action_simulator_tester.printInternalKBFacts();
-//     std::string action_name3 = "get_down_from_car";
-//     std::vector<std::string> params3 = {"batdad","car","ben_school"}; // person, car, location
-//     if(action_simulator_tester.revertActionStart(action_name3, params3))
-//         ROS_DEBUG("action reverted succesfully");
-//     else
-//         ROS_ERROR("failed to revert action");
-//     ROS_DEBUG("KB facts after reverting action:");
-//     action_simulator_tester.printInternalKBFacts();
