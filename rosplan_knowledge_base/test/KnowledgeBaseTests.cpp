f1a1908cd96f3b28ca5cfe413edf2bb9f18df5f4
Martin <martinkoling@gmail.com>
2019-04-02 19:06:25 +0100

    Completed tests for all domain services

--- rosplan_knowledge_base/test/KnowledgeBaseTests.cpp
+++ rosplan_knowledge_base/test/KnowledgeBaseTests.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 #include <typeinfo>
 #include <gtest/gtest.h>
 #include <ros/package.h>
@@ -12,13 +13,14 @@
 
 #include "rosplan_knowledge_base/KnowledgeBase.h"
 #include "rosplan_knowledge_msgs/GetDomainNameService.h"
-#include "rosplan_knowledge_msgs/GetAttributeService.h"
+#include "rosplan_knowledge_msgs/GetDomainAttributeService.h"
 #include "rosplan_knowledge_msgs/KnowledgeItem.h"
 #include "rosplan_knowledge_msgs/GetDomainOperatorService.h"
+#include "rosplan_knowledge_msgs/GetDomainOperatorDetailsService.h"
+#include "rosplan_knowledge_msgs/GetDomainPredicateDetailsService.h"
+#include "rosplan_knowledge_msgs/GetDomainTypeService.h"
 #include "rosplan_knowledge_msgs/DomainFormula.h"
 
-
-//KnowledgeBase *knowledge_base_tester;
 
 GTEST_TEST(KnowledgeBaseTests, Test1_domain_name) {
 
@@ -33,61 +35,128 @@ GTEST_TEST(KnowledgeBaseTests, Test1_domain_name) {
 
 }
 
+
+
 GTEST_TEST(KnowledgeBaseTests, Test2_domain_functions) {
 
     ros::NodeHandle n("~");
 
-    ros::ServiceClient client1 = n.serviceClient<rosplan_knowledge_msgs::GetAttributeService>("/rosplan_knowledge_base/domain/functions");
-    rosplan_knowledge_msgs::GetAttributeService srv;
+    ros::ServiceClient client1 = n.serviceClient<rosplan_knowledge_msgs::GetDomainAttributeService>("/rosplan_knowledge_base/domain/functions");
+    rosplan_knowledge_msgs::GetDomainAttributeService srv;
 
     client1.call(srv);
 
-    //rosplan_knowledge_msgs::KnowledgeItem response = srv.response.attributes;
+    // Amazon-domain does not contain any functions
 
-    std::cout << typeid(srv.response.attributes).name() << std::endl;
-
-
-    ASSERT_EQ(1 , 0);
-    //ASSERT_EQ("[]", srv.response.attributes);
+    ASSERT_EQ(0, srv.response.items.size());
 
 }
 
-/*
-GTEST_TEST(KnowledgeBaseTests, Test3_domain_operators) {
+GTEST_TEST(KnowledgeBaseTests, Test3_domain_predicates) {
 
-    /*
+    ros::NodeHandle n("~");
+
+    ros::ServiceClient client1 = n.serviceClient<rosplan_knowledge_msgs::GetDomainAttributeService>("/rosplan_knowledge_base/domain/predicates");
+    rosplan_knowledge_msgs::GetDomainAttributeService srv;
+
+    std::vector<std::string> expected_predicate_names = {"at", "in", "link", "path"};
+
+    client1.call(srv);
+
+    std::vector<std::string> actual_predicate_names;
+
+    for (auto rit=srv.response.items.begin(); rit!=srv.response.items.end(); rit++) {
+        actual_predicate_names.push_back(rit->name);
+    }
+
+    ASSERT_EQ(expected_predicate_names, actual_predicate_names);
+
+}
+
+
+GTEST_TEST(KnowledgeBaseTests, Test4_domain_operators) {
+
+
     ros::NodeHandle n("~");
 
     ros::ServiceClient client1 = n.serviceClient<rosplan_knowledge_msgs::GetDomainOperatorService>("/rosplan_knowledge_base/domain/operators");
     rosplan_knowledge_msgs::GetDomainOperatorService srv;
 
+    std::vector<std::string> expected_operator_names = {"load-truck", "unload-truck", "board-truck", "get-out", "drive-truck", "walk"};
+
     client1.call(srv);
 
-    std::vector<std::string> expected_operator_names = srv.response.operators;
+    std::vector<std::string> actual_operator_names;
 
+    for (auto rit=srv.response.operators.begin(); rit!=srv.response.operators.end(); rit++) {
+        actual_operator_names.push_back(rit->name);
+    }
 
-
-    std::vector<std::string> operator_names;
-    knowledge_base_tester->getOperatorNames(operator_names);
-
-    std::vector<std::string> expected_operator_names = {"goto_waypoint", "localise", "dock", "undock", \
-        "wait_load_at_machine", "switch_machine_on", "wait_unload", "ditch"};
-
-
-
-
-    ASSERT_EQ(operator_names , expected_operator_names);
-
+    ASSERT_EQ(expected_operator_names, actual_operator_names);
 
 }
-*/
+
+GTEST_TEST(KnowledgeBaseTests, Test5_domain_types){
+
+    ros::NodeHandle n("~");
+
+    ros::ServiceClient client1 = n.serviceClient<rosplan_knowledge_msgs::GetDomainTypeService>("/rosplan_knowledge_base/domain/types");
+    rosplan_knowledge_msgs::GetDomainTypeService srv;
+
+    std::vector<std::string> expected_type_names = {"place", "locatable", "driver", "truck", "item"};
+    std::vector<std::string> expected_super_type_names = {"object", "object", "locatable", "locatable", "locatable"};
+
+    client1.call(srv);
+
+    std::vector<std::string> actual_type_names = srv.response.types;
+    std::vector<std::string> actual_super_type_names = srv.response.super_types;
+
+    ASSERT_EQ(expected_type_names, actual_type_names);
+    ASSERT_EQ(expected_super_type_names, actual_super_type_names);
+
+}
+
+GTEST_TEST(KnowledgeBaseTests, Test6_domain_operator_details_load_truck) {
+
+    ros::NodeHandle n("~");
+
+    ros::ServiceClient client1 = n.serviceClient<rosplan_knowledge_msgs::GetDomainOperatorDetailsService>("/rosplan_knowledge_base/domain/operator_details");
+    rosplan_knowledge_msgs::GetDomainOperatorDetailsService srv;
+
+    srv.request.name = "load-truck";
+
+    client1.call(srv);
+
+    std::string detailed_operator_name = srv.response.op.formula.name;
+
+    ASSERT_EQ(srv.request.name, detailed_operator_name);
+
+}
+
+GTEST_TEST(KnowledgeBaseTests, Test7_domain_predicate_details_path) {
+
+    ros::NodeHandle n("~");
+
+    ros::ServiceClient client1 = n.serviceClient<rosplan_knowledge_msgs::GetDomainPredicateDetailsService>("/rosplan_knowledge_base/domain/predicate_details");
+    rosplan_knowledge_msgs::GetDomainPredicateDetailsService srv;
+
+    srv.request.name = "path";
+
+    client1.call(srv);
+
+    std::string detailed_predicate_name = srv.response.predicate.name;
+
+    ASSERT_EQ(srv.request.name, detailed_predicate_name);
+
+}
+
+// for report: test more thoroughly functions, predicates, operators. Deeper testing with operator_details and predicate_deatails. Update picture, missing service: predicate_details.
 
 int main(int argc, char **argv) {
 
     ::testing::InitGoogleTest(&argc, argv);
     ros::init(argc, argv, "KnowledgeBaseTests");
 
-    // knowledge_base_tester = new KnowledgeBase();
 
     return RUN_ALL_TESTS();
 }
