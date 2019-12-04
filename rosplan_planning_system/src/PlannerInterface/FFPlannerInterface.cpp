af7ca5742948870df3ad8a6f717b36f94f8d5893
Tom Creutz <tcreutz97@gmail.com>
2019-12-04 10:56:48 +0100

    add use_ffha planner interface param to ensure compatibility with ff and ffha

--- rosplan_planning_system/src/PlannerInterface/FFPlannerInterface.cpp
+++ rosplan_planning_system/src/PlannerInterface/FFPlannerInterface.cpp
@@ -17,6 +17,8 @@ namespace KCL_rosplan {
 		node_handle->getParam("planner_topic", plannerTopic);
 		plan_publisher = node_handle->advertise<std_msgs::String>(plannerTopic, 1, true);
 
+		node_handle->param<bool>("use_ffha", this->use_ffha, false);
+
 		// start planning action server
 		plan_server->start();
 	}
@@ -86,19 +88,36 @@ namespace KCL_rosplan {
 
 		// Parse the solved plan
 		if (solved) {
-			std::getline(planfile, line); //go to first action line
+			if (this->use_ffha)
+			{
+				std::getline(planfile, line); //go to first action line
+			} else {
+			// actions look like this:
+			// step    0: got_place C1
+			//         1: find_object V1 C1
+			// plan cost: XX
+				while (std::getline(planfile, line)) { // Move to the beginning of the plan
+					if (line.substr(0, 4) == "step") {
+						line = line.substr(4); // Remove the step
+						break;
+					}
+				}
+			}
+
 			// First iteration line will be like   0: got_place C1
-			while (line.find("cost of plan") == line.npos and line.find("time spend") == line.npos and line.size() > 0) {
+			while (line.find("Total cost") == line.npos and line.find("time spend") == line.npos and line.size() > 0) {
 				std::stringstream ss(line); // To trim whitespaces
 				std::string aux;
 				// Read the action number X:
 				ss >> aux;
-				planner_output += aux + " "; // Add parenthesis before the action
+				std::string add = (use_ffha) ? " " : " (";
+				planner_output += aux + add; // Add parenthesis before the action
 				while (ss >> aux) { // Read the rest
 					planner_output += aux;
 					if (ss.good()) planner_output += " "; // Add a whitespace unless we have processed all the line
 				}
-				planner_output += "  [0.001]\n"; // Close parenthesis and add duration
+				add = (use_ffha) ? "  [0.001]\n" : ")  [0.001]\n";
+				planner_output += add; // Close parenthesis and add duration
 				std::getline(planfile, line);
 			}
 			// Convert to lowercase as FF prints all the actions and parameters in uppercase
