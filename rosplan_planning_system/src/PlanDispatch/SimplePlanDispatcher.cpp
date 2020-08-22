6e9f788408a3de99822617955bc0626a663ee2f7
Stefan Bezrucav <bezrucav@igmr.rwth-aachen.de>
2020-08-22 11:18:04 +0200

    MOD: use the statuses defined in the FeedbackMsg both in the old action interfaces and in the plan dispatchers; some more bugs resolved due to this change

--- rosplan_planning_system/src/PlanDispatch/SimplePlanDispatcher.cpp
+++ rosplan_planning_system/src/PlanDispatch/SimplePlanDispatcher.cpp
@@ -147,7 +147,7 @@ namespace KCL_rosplan {
 	void SimplePlanDispatcher::feedbackCallback(const rosplan_dispatch_msgs::ActionFeedback::ConstPtr& msg) {
 
 		// create error if the action is unrecognised
-		ROS_INFO("KCL: (%s) Feedback received [%i, %d]", ros::this_node::getName().c_str(), msg->action_id, msg->status.c_str());
+		ROS_INFO("KCL: (%s) Feedback received [%i, %d]", ros::this_node::getName().c_str(), msg->action_id, msg->status);
 		if(current_action != (unsigned int)msg->action_id)
 			ROS_ERROR("KCL: (%s) Unexpected action ID: %d; current action: %d", ros::this_node::getName().c_str(), msg->action_id, current_action);
 
