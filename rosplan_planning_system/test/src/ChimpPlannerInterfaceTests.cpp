2b2cfc8ac1b87dd9238cbb6354b91340853d94bf
Sebastian Stock <sebastian.stock@gmail.com>
2020-09-07 14:42:40 +0200

    Add test for CHIMPPlannerInterface
    Test for invalid problem file

--- rosplan_planning_system/test/src/ChimpPlannerInterfaceTests.cpp
+++ rosplan_planning_system/test/src/ChimpPlannerInterfaceTests.cpp
@@ -47,10 +47,7 @@ GTEST_TEST(PlannerInterfaceTests, Test1_plan_found) {
     rosplan_dispatch_msgs::PlanningService srv = createDefaultRequest();
 
     ros::service::waitForService(srv_name, ros::Duration(3));
-    bool is_srv_call_successful = false;
-    if(client.call(srv))
-        is_srv_call_successful = true;
-    
+    bool is_srv_call_successful = client.call(srv);
     EXPECT_TRUE(is_srv_call_successful);
     EXPECT_TRUE(srv.response.plan_found);
 }
@@ -107,34 +104,24 @@ GTEST_TEST(PlannerInterfaceTests, Test3_problem_without_solution) {
     srv.request.problem_path = srv.request.data_path + "/test_problem_no_solution.pdl";
 
     ros::service::waitForService(srv_name, ros::Duration(3));
-    bool is_srv_call_successful = false;
-    if(client.call(srv))
-        is_srv_call_successful = true;
-
+    bool is_srv_call_successful = client.call(srv);
     EXPECT_TRUE(is_srv_call_successful);
     EXPECT_FALSE(srv.response.plan_found);
 }
 
-// GTEST_TEST(PlannerInterfaceTests, Test4_invalid_pddl_syntax) { // TODO invalid CHIMP domain syntax
+GTEST_TEST(PlannerInterfaceTests, Test4_invalid_problem_syntax) {
 
-//     ros::NodeHandle nh("~");
+    ros::NodeHandle nh("~");
 
-//     std::string srv_name = "/rosplan_planner_interface/planning_server_params";
-//     ros::ServiceClient client1 = nh.serviceClient<rosplan_dispatch_msgs::PlanningService>(srv_name);
-//     rosplan_dispatch_msgs::PlanningService srv;
+    std::string srv_name = "/rosplan_planner_interface/planning_server_params";
+    ros::ServiceClient client1 = nh.serviceClient<rosplan_dispatch_msgs::PlanningService>(srv_name);
+    rosplan_dispatch_msgs::PlanningService srv = createDefaultRequest();
+    srv.request.problem_path = srv.request.data_path + "/test_problem_invalid_syntax.pdl";
 
-//     std::string rosplan_planning_system_path = ros::package::getPath("rosplan_planning_system");
-
-//     srv.request.use_problem_topic = false;
-//     srv.request.data_path = rosplan_planning_system_path + "/test/pddl/test_domain/";
-//     srv.request.domain_path = rosplan_planning_system_path + "/test/pddl/test_domain/domain.pddl";
-//     srv.request.problem_path = rosplan_planning_system_path + "/test/pddl/test_domain/test_problem_invalid_syntax.pddl";
-//     srv.request.planner_command = "timeout 10 " + rosplan_planning_system_path + "/common/bin/popf -n DOMAIN PROBLEM";
-
-//     ros::service::waitForService(srv_name, ros::Duration(3));
-//     EXPECT_TRUE(client1.call(srv));
-//     EXPECT_FALSE(srv.response.plan_found);
-// }
+    ros::service::waitForService(srv_name, ros::Duration(3));
+    EXPECT_TRUE(client1.call(srv));
+    EXPECT_FALSE(srv.response.plan_found);
+}
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv) {
