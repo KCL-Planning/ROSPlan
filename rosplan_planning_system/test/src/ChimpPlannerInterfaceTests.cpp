83e6bce81b0794a8e8174e22678bfef028f44ba1
Sebastian Stock <sebastian.stock@gmail.com>
2020-09-07 11:51:58 +0200

    Add test for problem without solution for CHIMP

--- rosplan_planning_system/test/src/ChimpPlannerInterfaceTests.cpp
+++ rosplan_planning_system/test/src/ChimpPlannerInterfaceTests.cpp
@@ -25,6 +25,17 @@ void testCallback(const std_msgs::String::ConstPtr &plan) {
     plan_received = true;
 }
 
+rosplan_dispatch_msgs::PlanningService createDefaultRequest() {
+    rosplan_dispatch_msgs::PlanningService srv;
+    srv.request.use_problem_topic = false;
+    srv.request.data_path = ros::package::getPath("rosplan_planning_system") + "/test/hierarchical_domains/chimp/restaurant";
+    srv.request.domain_path = srv.request.data_path + "/race_waiter_domain.ddl";
+    srv.request.problem_path = srv.request.data_path + "/serve_coffee_problem.pdl";
+    std::string chimp_path = ros::package::getPath("rosplan_dependencies") + "/chimp"; 
+    srv.request.planner_command = "timeout 3 " + chimp_path + "/gradlew run -p " + chimp_path + " -Dexec.args=\"DOMAIN PROBLEM -o=OUTPUT\"";
+    return srv;
+}
+
 
 GTEST_TEST(PlannerInterfaceTests, Test1_plan_found) {
 
@@ -32,16 +43,8 @@ GTEST_TEST(PlannerInterfaceTests, Test1_plan_found) {
 
     std::string srv_name = "/rosplan_planner_interface/planning_server_params";
     ros::ServiceClient client = nh.serviceClient<rosplan_dispatch_msgs::PlanningService>(srv_name);
-    rosplan_dispatch_msgs::PlanningService srv;
 
-    std::string rosplan_planning_system_path = ros::package::getPath("rosplan_planning_system"); 
-    std::string chimp_path = ros::package::getPath("rosplan_dependencies") + "/chimp"; 
-
-    srv.request.use_problem_topic = false;
-    srv.request.data_path = rosplan_planning_system_path + "/test/hierarchical_domains/chimp/restaurant";
-    srv.request.domain_path = rosplan_planning_system_path + "/test/hierarchical_domains/chimp/restaurant/race_waiter_domain.ddl";
-    srv.request.problem_path = rosplan_planning_system_path + "/test/hierarchical_domains/chimp/restaurant/serve_coffee_problem.pdl";
-    srv.request.planner_command = "timeout 10 " + chimp_path + "/gradlew run -p " + chimp_path + " -Dexec.args=\"DOMAIN PROBLEM -o=OUTPUT\"";
+    rosplan_dispatch_msgs::PlanningService srv = createDefaultRequest();
 
     ros::service::waitForService(srv_name, ros::Duration(3));
     bool is_srv_call_successful = false;
@@ -61,20 +64,13 @@ GTEST_TEST(PlannerInterfaceTests, Test2_format_published_on_planner_output) {
 
     std::string srv_name = "/rosplan_planner_interface/planning_server_params";
     ros::ServiceClient client1 = nh.serviceClient<rosplan_dispatch_msgs::PlanningService>(srv_name);
-    rosplan_dispatch_msgs::PlanningService srv;
 
-    std::string rosplan_planning_system_path = ros::package::getPath("rosplan_planning_system");
-    std::string chimp_path = ros::package::getPath("rosplan_dependencies") + "/chimp";
-
+    rosplan_dispatch_msgs::PlanningService srv = createDefaultRequest();
     srv.request.use_problem_topic = true;
-    srv.request.data_path = rosplan_planning_system_path + "/test/hierarchical_domains/chimp/restaurant";
-    srv.request.domain_path = rosplan_planning_system_path + "/test/hierarchical_domains/chimp/restaurant/race_waiter_domain.ddl";
-    // The planner interface **writes** the problem to the following file:
-    srv.request.problem_path = rosplan_planning_system_path + "/test/hierarchical_domains/chimp/restaurant/generated_problem.pdl";
-    srv.request.planner_command = "timeout 10 " + chimp_path + "/gradlew run -p " + chimp_path + " -Dexec.args=\"DOMAIN PROBLEM -o=OUTPUT\"";
+    srv.request.problem_path = srv.request.data_path + "/generated_problem.pdl"; // the planner interface **writes** the problem to this file
 
     // Input problem that is send via the topic
-    std::ifstream t(rosplan_planning_system_path + "/test/hierarchical_domains/chimp/restaurant/test_m_drive_robot_1.pdl");
+    std::ifstream t(srv.request.data_path + "/test_m_drive_robot_1.pdl");
     std::stringstream ss;
     ss << t.rdbuf();
     std_msgs::String msg;
@@ -100,32 +96,26 @@ GTEST_TEST(PlannerInterfaceTests, Test2_format_published_on_planner_output) {
     ASSERT_EQ(last_plan, known_plan);
 }
 
-// GTEST_TEST(PlannerInterfaceTests, Disable_Test3_problem_without_solution) {
+GTEST_TEST(PlannerInterfaceTests, Test3_problem_without_solution) {
 
-//     ros::NodeHandle nh("~");
+    ros::NodeHandle nh("~");
 
-//     std::string srv_name = "/rosplan_planner_interface/planning_server_params";
-//     ros::ServiceClient client = nh.serviceClient<rosplan_dispatch_msgs::PlanningService>(srv_name);
-//     rosplan_dispatch_msgs::PlanningService srv;
+    std::string srv_name = "/rosplan_planner_interface/planning_server_params";
+    ros::ServiceClient client = nh.serviceClient<rosplan_dispatch_msgs::PlanningService>(srv_name);
 
-//     std::string rosplan_planning_system_path = ros::package::getPath("rosplan_planning_system");
+    rosplan_dispatch_msgs::PlanningService srv = createDefaultRequest();
+    srv.request.problem_path = srv.request.data_path + "/test_problem_no_solution.pdl";
 
-//     srv.request.use_problem_topic = false;
-//     srv.request.data_path = rosplan_planning_system_path + "/test/pddl/test_domain/";
-//     srv.request.domain_path = rosplan_planning_system_path + "/test/pddl/test_domain/domain.pddl";
-//     srv.request.problem_path = rosplan_planning_system_path + "/test/pddl/test_domain/test_problem_no_solution.pddl";
-//     srv.request.planner_command = "timeout 10 " + rosplan_planning_system_path + "/common/bin/popf -n DOMAIN PROBLEM";
+    ros::service::waitForService(srv_name, ros::Duration(3));
+    bool is_srv_call_successful = false;
+    if(client.call(srv))
+        is_srv_call_successful = true;
 
-//     ros::service::waitForService(srv_name, ros::Duration(3));
-//     bool is_srv_call_successful = false;
-//     if(client.call(srv))
-//         is_srv_call_successful = true;
+    EXPECT_TRUE(is_srv_call_successful);
+    EXPECT_FALSE(srv.response.plan_found);
+}
 
-//     EXPECT_TRUE(is_srv_call_successful);
-//     EXPECT_FALSE(srv.response.plan_found);
-// }
-
-// GTEST_TEST(PlannerInterfaceTests, Disable_Test4_invalid_pddl_syntax) { // TODO invalid CHIMP domain syntax
+// GTEST_TEST(PlannerInterfaceTests, Test4_invalid_pddl_syntax) { // TODO invalid CHIMP domain syntax
 
 //     ros::NodeHandle nh("~");
 
