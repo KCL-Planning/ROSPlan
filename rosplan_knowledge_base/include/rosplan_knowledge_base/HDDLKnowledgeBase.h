5d326384e80fe5b0c6bffc01adeec8cba2382141
Oscar Lima <oscar.lima_carrion@dfki.de>
2020-09-29 18:13:17 +0200

    implement virtual functions from KB using hddl parsed information

--- rosplan_knowledge_base/include/rosplan_knowledge_base/HDDLKnowledgeBase.h
+++ rosplan_knowledge_base/include/rosplan_knowledge_base/HDDLKnowledgeBase.h
@@ -9,7 +9,7 @@ namespace KCL_rosplan {
 
     class HDDLKnowledgeBase : public KnowledgeBase {
       public:
-        HDDLKnowledgeBase(ros::NodeHandle& n) : KnowledgeBase(n) {};
+        HDDLKnowledgeBase(ros::NodeHandle& n);
 
         /* parse domain and probelm files */
         void parseDomain(const std::string& domain_file_path, const std::string& problem_file_path) override;
@@ -22,10 +22,14 @@ namespace KCL_rosplan {
         bool getDomainName(rosplan_knowledge_msgs::GetDomainNameService::Request  &req, rosplan_knowledge_msgs::GetDomainNameService::Response &res) override;
         bool getTypes(rosplan_knowledge_msgs::GetDomainTypeService::Request  &req, rosplan_knowledge_msgs::GetDomainTypeService::Response &res) override;
         bool getPredicates(rosplan_knowledge_msgs::GetDomainAttributeService::Request  &req, rosplan_knowledge_msgs::GetDomainAttributeService::Response &res) override;
+        bool getPredicateDetails(rosplan_knowledge_msgs::GetDomainPredicateDetailsService::Request  &req, rosplan_knowledge_msgs::GetDomainPredicateDetailsService::Response &res) override;
         bool getFunctionPredicates(rosplan_knowledge_msgs::GetDomainAttributeService::Request  &req, rosplan_knowledge_msgs::GetDomainAttributeService::Response &res) override;
         bool getOperators(rosplan_knowledge_msgs::GetDomainOperatorService::Request  &req, rosplan_knowledge_msgs::GetDomainOperatorService::Response &res) override;
         bool getOperatorDetails(rosplan_knowledge_msgs::GetDomainOperatorDetailsService::Request  &req, rosplan_knowledge_msgs::GetDomainOperatorDetailsService::Response &res) override;
-        bool getPredicateDetails(rosplan_knowledge_msgs::GetDomainPredicateDetailsService::Request  &req, rosplan_knowledge_msgs::GetDomainPredicateDetailsService::Response &res) override;
+
       private:
+
+        HDDLParser hddl_parser_;
+        std::string problem_file_path_;
     };
 }
--- rosplan_knowledge_base/src/HDDLKnowledgeBase.cpp
+++ rosplan_knowledge_base/src/HDDLKnowledgeBase.cpp
@@ -1,23 +1,23 @@
 #include "rosplan_knowledge_base/HDDLKnowledgeBase.h"
 
 namespace KCL_rosplan {
-//     HDDLKnowledgeBase (ros::NodeHandle& n) {
-//         
-//     }
+    HDDLKnowledgeBase::HDDLKnowledgeBase(ros::NodeHandle& n) : KnowledgeBase(n), problem_file_path_(std::string("")) {}
 
     void HDDLKnowledgeBase::parseDomain(const std::string &domain_file_path, const std::string &problem_file_path) {
-//         domain_path_ = domain_file_path;
 
-        // TODO parse HDDL domain
-//         if (?) {
-//             ROS_ERROR("OTPEL: (%s) There were syntax errors in the domain or instance file.",
-//                       ros::this_node::getName().c_str());
-//             ros::shutdown();
-//         }
+        problem_file_path_ = problem_file_path;
 
-        if (problem_file_path != "")
-            addInitialState();
-        ROS_INFO("Parsing the domain...");
+        ROS_INFO("KCL: Parsing hddl domain : %s", domain_file_path.c_str());
+        hddl_parser_.parse(domain_file_path);
+
+        if (!hddl_parser_.parsing_ok()) {
+            ROS_ERROR("KCL: (%s) There were syntax errors in the domain file.",
+                      ros::this_node::getName().c_str());
+            ros::shutdown();
+        }
+
+        if (problem_file_path_ != "") addInitialState();
+        ROS_INFO("KCL: successfully parsed hddl domain!");
     }
 
     void HDDLKnowledgeBase::addInitialState() {
@@ -32,90 +32,80 @@ namespace KCL_rosplan {
 //         }
 
         // model instances -> std::map<std::string, std::vector<std::string> >
-        std::vector<std::string> robots;
-        robots.push_back("tiago");
-        model_instances.insert(std::pair<std::string, std::vector<std::string>>("robot", robots));
+//         std::vector<std::string> robots;
+//         robots.push_back("tiago");
+//         model_instances.insert(std::pair<std::string, std::vector<std::string>>("robot", robots));
+        ROS_WARN("Initial state not yet implemented, cannot load: %s", problem_file_path_.c_str());
     }
 
     void HDDLKnowledgeBase::addConstants() {}
 
     bool HDDLKnowledgeBase::getDomainName(rosplan_knowledge_msgs::GetDomainNameService::Request  &req, rosplan_knowledge_msgs::GetDomainNameService::Response &res) {
-        res.domain_name = "turtlebot"; // TODO: set proper name from hddl file
+        res.domain_name = hddl_parser_.domain_.domain_name_;
         return true;
     }
 
     bool HDDLKnowledgeBase::getTypes(rosplan_knowledge_msgs::GetDomainTypeService::Request  &req, rosplan_knowledge_msgs::GetDomainTypeService::Response &res) {
-        res.types.push_back("waypoint");
-        res.types.push_back("robot");
-//      res.super_types
+        for(auto it = hddl_parser_.domain_.domain_types_.begin(); it!=hddl_parser_.domain_.domain_types_.end(); it++) {
+            res.types.push_back(it->first);
+            res.super_types.push_back(it->second);
+        }
         return true;
     }
 
     bool HDDLKnowledgeBase::getPredicates(rosplan_knowledge_msgs::GetDomainAttributeService::Request  &req, rosplan_knowledge_msgs::GetDomainAttributeService::Response &res) {
-        rosplan_knowledge_msgs::DomainFormula df;
-        diagnostic_msgs::KeyValue kv;
-        
-        df.name =  "robot_at";
-        kv.key =  "v";
-        kv.value =  "robot";
-        df.typed_parameters.push_back(kv);
-        kv.key =  "wp";
-        kv.value =  "waypoint";
-        df.typed_parameters.push_back(kv);
-        res.items.push_back(df);
-        df.typed_parameters.clear();
-        
-        df.name =  "visited";
-        kv.key =  "wp";
-        kv.value =  "waypoint";
-        df.typed_parameters.push_back(kv);
-        res.items.push_back(df);
-        df.typed_parameters.clear();
-        
-        df.name =  "undocked";
-        kv.key =  "v";
-        kv.value =  "robot";
-        df.typed_parameters.push_back(kv);
-        res.items.push_back(df);
-        df.typed_parameters.clear();
-        
-        df.name =  "docked";
-        kv.key =  "v";
-        kv.value =  "robot";
-        df.typed_parameters.push_back(kv);
-        res.items.push_back(df);
-        df.typed_parameters.clear();
-        
-        df.name =  "localised";
-        kv.key =  "v";
-        kv.value =  "robot";
-        df.typed_parameters.push_back(kv);
-        res.items.push_back(df);
-        df.typed_parameters.clear();
-        
-        df.name =  "dock_at";
-        kv.key =  "wp";
-        kv.value =  "waypoint";
-        df.typed_parameters.push_back(kv);
-        res.items.push_back(df);
-        df.typed_parameters.clear();
-        
-        return true;
-    }
 
-    bool HDDLKnowledgeBase::getFunctionPredicates(rosplan_knowledge_msgs::GetDomainAttributeService::Request  &req, rosplan_knowledge_msgs::GetDomainAttributeService::Response &res) {
-        return true;
-    }
+        for(auto it=hddl_parser_.domain_.domain_predicates_.begin(); it!=hddl_parser_.domain_.domain_predicates_.end(); it++) {
 
-    bool HDDLKnowledgeBase::getOperators(rosplan_knowledge_msgs::GetDomainOperatorService::Request  &req, rosplan_knowledge_msgs::GetDomainOperatorService::Response &res) {
-        return true;
-    }
+            rosplan_knowledge_msgs::DomainFormula df;
+            diagnostic_msgs::KeyValue kv;
 
-    bool HDDLKnowledgeBase::getOperatorDetails(rosplan_knowledge_msgs::GetDomainOperatorDetailsService::Request  &req, rosplan_knowledge_msgs::GetDomainOperatorDetailsService::Response &res) {
+            df.name =  it->name;
+            for(auto pit=it->pred_params.params.begin(); pit!=it->pred_params.params.end(); pit++) {
+                kv.key =  *pit;
+                kv.value = it->pred_params.params_map[*pit];
+                df.typed_parameters.push_back(kv);
+            }
+            res.items.push_back(df);
+        }
+
         return true;
     }
 
     bool HDDLKnowledgeBase::getPredicateDetails(rosplan_knowledge_msgs::GetDomainPredicateDetailsService::Request  &req, rosplan_knowledge_msgs::GetDomainPredicateDetailsService::Response &res) {
+
+        for(auto it=hddl_parser_.domain_.domain_predicates_.begin(); it!=hddl_parser_.domain_.domain_predicates_.end(); it++) {
+
+            if(it->name == req.name) {
+
+                rosplan_knowledge_msgs::DomainFormula df;
+                diagnostic_msgs::KeyValue kv;
+
+                df.name =  it->name;
+                for(auto pit=it->pred_params.params.begin(); pit!=it->pred_params.params.end(); pit++) {
+                    kv.key =  *pit;
+                    kv.value = it->pred_params.params_map[*pit];
+                    df.typed_parameters.push_back(kv);
+                }
+                res.predicate = df;
+            }
+        }
+
         return true;
+    }
+
+    bool HDDLKnowledgeBase::getFunctionPredicates(rosplan_knowledge_msgs::GetDomainAttributeService::Request  &req, rosplan_knowledge_msgs::GetDomainAttributeService::Response &res) {
+        ROS_WARN("Functions are not supported in hddl, returning empty response");
+        return false;
+    }
+
+    bool HDDLKnowledgeBase::getOperators(rosplan_knowledge_msgs::GetDomainOperatorService::Request  &req, rosplan_knowledge_msgs::GetDomainOperatorService::Response &res) {
+        ROS_WARN("Not implemented yet (getOperators)");
+        return false;
+    }
+
+    bool HDDLKnowledgeBase::getOperatorDetails(rosplan_knowledge_msgs::GetDomainOperatorDetailsService::Request  &req, rosplan_knowledge_msgs::GetDomainOperatorDetailsService::Response &res) {
+        ROS_WARN("Not implemented yet! (getOperatorDetails)");
+        return false;
     }
 }
