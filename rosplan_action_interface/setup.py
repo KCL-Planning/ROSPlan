255e52cf334dd30453f32ba05ef528b22da1ca11
michael <michael.cashmore@kcl.ac.uk>
2021-04-13 14:45:20 +0100

    Updated setup.py in action interface

--- rosplan_action_interface/setup.py
+++ rosplan_action_interface/setup.py
@@ -5,8 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # for your packages to be recognized by python
 d = generate_distutils_setup(
-  packages=['rosplan_planning_system'],
-  package_dir={'rosplan_planning_system': 'src/rosplan_planning_system'}
+  packages=['rosplan_action_interface'],
+  package_dir={'': 'src'}
 )
 
 setup(**d)
